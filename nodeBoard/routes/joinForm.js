e8dd5ee8813d1e16fb24971a355beabd5fac8de6
jcmlov <jcmlov@gmail.com>
2016-08-17 12:30:56 +0900

    mysql 연동

--- nodeBoard/routes/joinForm.js
+++ nodeBoard/routes/joinForm.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* Get users listing */
 router.get('/', function(req, res, next) {
-	res.send('정상적으로 작동하고 있습니다.');
+	res.render('joinForm', {title:'회원가입폼'});
+});
+
+router.post('/', function(req, res, next) {
+	console.log('req.body : ' + req.body);
+	res.json(req.body);
 });
 
 module.exports = router;
